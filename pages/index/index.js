4fcdc136e509401f40a06d52b87d7832208a717f
bigDragon <250899930@qq.com>
2019-02-26 13:35:05 +0800

    修改heqd背景图让其自适应

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,21 +3,24 @@ Page({
   onLoad: function () {
     console.log(app.globalData.token); //全局变量值的获取
   },
-  
+
   data: {
-    name:"123",
+    array: [{
+      mode: 'widthFix'
+    }],
+    src: 'https://www.goodb2b.cn/xdxd_file/head_top.jpg',
     imgUrls: [
       '../../images/discover_1.jpg',
       '../../images/discover_2.jpg',
       '../../images/discover_3.jpg'
     ],
-    indicatorDots: false,//是否显示指示点
-    vertical: false,//滑动方向是否为纵向
-    autoplay: true,//是否自动切换
-    circular: false,//是否采用衔接滑动
-    interval: 2000,//自动切换时间间隔
-    duration: 500,//滑动动画时长
-    previousMargin: 0,//前边距，可用于露出前一项的一小部分，接受 px 和 rpx 值
-    nextMargin: 0//后边距，可用于露出后一项的一小部分，接受 px 和 rpx 值
+    indicatorDots: false, //是否显示指示点
+    vertical: false, //滑动方向是否为纵向
+    autoplay: true, //是否自动切换
+    circular: false, //是否采用衔接滑动
+    interval: 2000, //自动切换时间间隔
+    duration: 500, //滑动动画时长
+    previousMargin: 0, //前边距，可用于露出前一项的一小部分，接受 px 和 rpx 值
+    nextMargin: 0 //后边距，可用于露出后一项的一小部分，接受 px 和 rpx 值
   }
 })
